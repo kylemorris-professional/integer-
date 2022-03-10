9e8f02572c93586430aaec2912b55b74de6df9c9
Kyle-Anthony Morris <kylemorris.professional@gmail.com>
2022-03-10 11:40:05 -0500

    after asking question, input promt apears on new line

--- intiger.py
+++ intiger.py
@@ -1,7 +1,10 @@
-num = input("please enter a number: ")
-num = int(num)
-
-if num < 0:
-    print("this is a negative number")
-elif num > 0:
-    print("this is a postive number")
+while True:
+    print("please enter a number: ")
+    num = input()
+    num = int(num)
+    if num < 0:
+        print("this is a negative number")
+        continue
+    elif num > 0:
+        print("this is a postive number")
+        continue
