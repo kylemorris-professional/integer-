694bb8de919b069fff021e2ded945c898fd1fc6b
Kyle-Anthony Morris <kylemorris.professional@gmail.com>
2022-03-10 11:54:14 -0500

    second iteration

--- intiger.py
+++ intiger.py
@@ -1,10 +1,16 @@
-while True:
-    print("please enter a number: ")
-    num = input()
-    num = int(num)
-    if num < 0:
-        print("this is a negative number")
-        continue
-    elif num > 0:
-        print("this is a postive number")
-        continue
+#this is a program that tells you if the number you have entered is a postive interger or a negative interager or a zero 
+num = 0
+num = input("please enter a number: ")
+print("\n")
+num = int(num)
+
+while num != 0:
+    if num > 0:
+        print(num, "is a postive number")
+        num = 0
+
+    elif num < 0:
+        print(num, "is a negative number")
+        num = 0
+
+
